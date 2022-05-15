eb1f010fad224e1a04d12a7f7bbc4b669e80a8df
Lucas Freitas <mrlucasfreitas1@gmail.com>
2022-05-15 18:23:00 -0300

    Fix

--- ansible-stack/scripts/test-sites.sh
+++ ansible-stack/scripts/test-sites.sh
@@ -4,7 +4,7 @@ eipsfile="terraform-aws-ec2-instance/info/eips.txt"
 
 for x in $(cat ${eipsfile}); do
   if [ $(curl -LI http://$x -o /dev/null -w '%{http_code}\n' -s) == "200" ]; then
-    echo "\033[32m[OK] - http://$x\033[0m";
+    echo -e "\\e[01;32m[OK] - http://$x\\e[00m"
   else
     echo "\033[31m[ERROR] - http://$x\033[0m";
   fi
